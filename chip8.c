3dd2e21093a4a08197bf4f450c13451c025fa8fc
ZegarekD <davivipe@hotmail.com>
2024-11-14 12:31:32 -0300

    Window renderer added

--- chip8.c
+++ chip8.c
@@ -1,37 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdbool.h>
 //#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h> //#include "SDL.h"
 
+typedef struct
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+} sdl_t;
+
+typedef struct
+{
+    uint32_t window_width;  // SDL Window width (largura)
+    uint32_t window_height; // SDL Window height (altura)
+    uint32_t fg_color;      // Foregroud Color RGBA8888
+    uint32_t bg_color;      // Foregroud Color RGBA8888
+    uint32_t scale_factor;  // Amount to scale a CHIP8 by e. g. 20x will be a 20x larger window
+} config_t;
+
 //Initalize SDL
-bool init_sdl(void)
+bool init_sdl(sdl_t *sdl, const config_t config)
 {
     if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0)
     {
         SDL_Log("Could not initialize SDL subsystems! %s\n", SDL_GetError());
         return false;
     }
+
+    sdl -> window = SDL_CreateWindow("CHIP8 Emulator", SDL_WINDOWPOS_CENTERED, 
+                                    SDL_WINDOWPOS_CENTERED,
+                                    config.window_width *config.scale_factor,
+                                    config.window_height *config.scale_factor, 
+                                    0);
+    if(!sdl->window)
+    {
+        SDL_Log("Could not create SDL window %s\n", SDL_GetError());
+        return false;
+    }
+
+    sdl->renderer = SDL_CreateRenderer(sdl->window, -1, SDL_RENDERER_ACCELERATED);
+    if(!sdl->renderer)
+    {
+        SDL_Log("Could not create SDL renderer %s\n", SDL_GetError());
+    }
+
     return true; //Success
 }
 
-// Final cleanup
-void final_cleanup(void)
+bool set_config_from_args(config_t *config, const int argc, char **argv)
 {
+    //set defaults
+    *config = (config_t)
+    {
+        .window_width   = 64, //resolução original do chip8
+        .window_height  = 32,
+        //.fg_color = 0xFFFF00FF, // Yellow
+        //.bg_color = 0x00000000, // Black
+        .fg_color = 0xFFFFFFFF, // White
+        .bg_color = 0xFFFF00FF, // Yellow
+        .scale_factor = 20, // Default resolution will be 1280x640
+    };
+
+    //override defaults from passed in arguments
+    for(int i = 0; i < argc; i++)
+    {
+        (void)argv[i]; //prevent compile error from unused variables argc/argv
+        // ...
+    }
+    return true;
+}
+
+// Final cleanup
+void final_cleanup(const sdl_t sdl)
+{
+    SDL_DestroyRenderer(sdl.renderer);
+    SDL_DestroyWindow(sdl.window);
     SDL_Quit(); //Shut down SDL subsystems
+}
+
+void clear_screen(const sdl_t sdl, const config_t config)
+{
+    const uint8_t r = (config.bg_color >> 24) & 0xFF;
+    const uint8_t g = (config.bg_color >> 16) & 0xFF;
+    const uint8_t b = (config.bg_color >>  8) & 0xFF;
+    const uint8_t a = (config.bg_color >>  0) & 0xFF;
+
+    SDL_SetRenderDrawColor(sdl.renderer, r, g, b, a);
+    SDL_RenderClear(sdl.renderer);
+}
+
+void update_screen(sdl_t sdl)
+{
+    SDL_RenderPresent(sdl.renderer);
 }
 
 //a squeeze do mr. gui
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
+    //initialize emulator configuration/options
+    config_t config = {0};
+    if(!set_config_from_args(&config, argc, argv)) exit(EXIT_FAILURE);
 
     //initialize SDL
-    if(!init_sdl()) exit(EXIT_FAILURE);
+    sdl_t sdl = {0};
+    if(!init_sdl(&sdl, config)) exit(EXIT_FAILURE);
+
+    //initial scree..
+    clear_screen(sdl, config);
+
+    while(true)
+    {
+        // Get_time();
+        // Emulate CHIP8 Instructions
+        //Get_time(); elapsed since last get_time();
+        // Delay for 60hz/60fps
+        SDL_Delay(16);
+
+        // Update window with changes
+        update_screen(sdl);
+    }
 
     // Final cleanup
-    final_cleanup();
+    final_cleanup(sdl);
     //puts("Testando no GNU/Linux");
 
     exit(EXIT_SUCCESS);
